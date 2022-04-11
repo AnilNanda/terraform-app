d5ac0b971179bdf03b20868eed45e93c01a8a050
anilthekkoote@gmail.com <anilthekkoote@gmail.com>
2022-04-11 11:36:03 +0530

    Ansible provisioner in packer

--- packer_ami/installjenkins.sh
+++ packer_ami/installjenkins.sh
@@ -3,10 +3,8 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 sudo yum upgrade -y
 sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install -y jenkins
+sudo yum install -y jenkins git docker
 sudo systemctl enable jenkins
-sudo systemctl start jenkins
-
-sudo yum install git docker -y
+sudo systemctl enable docker
 
 sudo usermod -aG docker jenkins
